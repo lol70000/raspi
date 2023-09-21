f555a5238e81c74b948f8b709221c2e74dd1bdcd
lol70000 <tobiluchsinger@bluewin.ch>
2023-09-21 15:15:12 +0100

    hello

--- main.py
+++ main.py
@@ -14,16 +14,17 @@ raspipie.printing("hello")
 from newsapi import NewsApiClient
 import tkinter as tk
 
-window = tk.Tk()
-window.attributes("-fullscreen")
+window= tk.Tk()
+window.attributes("-fullscreen",True)
 
-widt= 800
+widt= 763
 heigh= 480
 
-frame = tk.Frame(master=window,width=int(widt),height=int(heigh),bg="red")
+frame = tk.Frame(master=window,width=int(widt),bg="red",height=int(heigh))
 frame.pack(fill=tk.BOTH)
 
-btn_ext= tk.Button(text="X",command=window.destroy,bg="red")
+btn_ext= tk.Button(master=window,text="X",bg="blue",command=window.destroy)
+btn_ext.place(x=widt,y=0)
 
 label = tk.Label(master=frame,text="Hello World", bg="red")
 label.place(x=widt/2, y=heigh/2)
