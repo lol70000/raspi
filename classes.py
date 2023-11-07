39f1d6a169bb38203cce09a583482c5472033e3a
lol70000 <tobiluchsinger@bluewin.ch>
2023-11-07 13:49:45 +0000

    Update

--- classes.py
+++ classes.py
@@ -1,32 +1,50 @@
 import tkinter as tk
 from newsapi import NewsApiClient
+import grovepi
+import threading
 from WorldTimeAPI import service as serv
 
 class FullscreenWindow:
-    def __init__(self, root,color,widt,heigt):
+    def __init__(self, root,color,widt,heigt,disp):
         self.root = root
         self.root.attributes('-fullscreen', True)
-        self.frame = tk.Frame(master=root,bg=color,width=widt,height=heigt)
-        self.frame.pack()
-        self.color = color
+        self.bgcolor = color
+        self.fgcolor = "white"
+        self.widt= widt
+        self.heigh = heigt
         self.buttonfont = ("Helvetica",12)
         self.fontsize = 12
         self.font = ("Helvetica",16)
-        self.create_destry_button(widt-83,0)
+        news = newsApi()
+        butt=button()
+        self.list = news.request(disp,4)
+        x = threading.Thread(target=self.makeinitialframe())
+        y= threading.Thread(target=butt.status())
+        x.start()
+        y.start()
+        if (y ==1):
+            self.settingsscreen()
+
+
+    def makeinitialframe(self):
+        self.frame = tk.Frame(master=self.root,bg=self.bgcolor,width=self.widt,height=self.heigh)
+        self.frame.pack()
+        self.create_destry_button(self.widt-83,0)
         
     def create_destry_button(self,widt,heigh):
-        button1 = tk.Button(master=self.frame, text="Button 1",bg=self.color, command=self.root.destroy,font=self.buttonfont)
-        button1.configure(highlightbackground=self.color,highlightcolor=self.color)
-        button1.place(x=widt,y=heigh)
+        button1 = tk.Button(master=self.frame, text="X",bg="red",fg=self.fgcolor, command=self.root.destroy,font=self.buttonfont)
+        button1.configure(highlightbackground=self.bgcolor,highlightcolor=self.fgcolor)
+        button1.place(x=widt+45,y=heigh)
+        self.add_all_infos(self.list,20,200)
 
     def add_label(self,text,xx,yy):
-        label = tk.Label(master=self.frame,text=str(text),bg=self.color,font=self.font)
+        label = tk.Label(master=self.frame,text=str(text),bg=self.bgcolor,fg=self.fgcolor,font=self.font)
         label.place(x=xx,y=yy)
 
-    def add_all_labels(self,list,xx,yy):
+    def add_all_infos(self,list,xx,yy):
         x=0
         y = yy
-        abst = self.fontsize/2
+        abst = self.fontsize
         if(len(list)%2 ==0):
             y+= abst/2
         m=0
@@ -35,11 +53,17 @@ class FullscreenWindow:
             y+=abst
             m+=1
         while(x<len(list)):
-            label = tk.Label(master=self.frame,text=str(list[x]),bg=self.color,font=self.font)
+            label = tk.Label(master=self.frame,text=str(list[x]),bg=self.bgcolor,fg=self.fgcolor,font=self.font)
             label.place(x=xx,y=y)
             y-=abst
             y-=self.fontsize
-            x+=1           
+            x+=1
+
+    def settingsscreen(self):
+        self.frame.destroy()
+        self.frame = tk.Frame(master=self.root,bg=self.bgcolor,width=self.widt,height=self.heigh)
+        self.frame.pack()
+        self.create_destry_button(self.widt-83,0)
 
 class newsApi:
     def __init__(self):
@@ -76,3 +100,15 @@ class clock:
         timeonly = dateotime[1].split('.')
         self.puretime = timeonly[0]
         print(self.puretime)
+
+class button:
+    def __init__(self):
+        grovepi.set_bus('RPI_1');
+        self.buttonPort = 2
+        grovepi.pinMode(self.buttonPort, "INPUT")
+
+    def status(self):
+        while(grovepi.digitalRead(self.buttonPort)==0):
+            self.status()
+            print(0)
+        return grovepi.digitalRead(buttonPort)
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-from classes import FullscreenWindow,newsApi,clock
+from classes import FullscreenWindow,newsApi,clock,button
 from newsapi import NewsApiClient
 import threading
 import tkinter as tk
-
+import grovepi
 from WorldTimeAPI import service as serv
+
+run = True
 
 clo = clock()
 
 disp=["author","title"]
 req = ["Formel 1","general"]
-news= newsApi()
-requests = news.request(disp,4)
-print(requests)
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = FullscreenWindow(root,"black",800,480)
-    app.add_all_labels(requests,40,240)
-    root.mainloop()
+root = tk.Tk()
+app = FullscreenWindow(root,"black",800,480,disp)
+root.mainloop()
